ec985088ec56691a2d373ca83e42d2d092172bd7
Marco Cortes <marco.cortes@kin.com>
2020-07-06 16:24:40 -0500

    add http client methods

--- lib/customerio_api/client.rb
+++ lib/customerio_api/client.rb
@@ -11,17 +11,29 @@ module CustomerioAPI
     end
 
     def message(message_id)
-      request(message_path(message_id))
+      request(:get, message_path(message_id))
     end
 
     def messages(limit: nil)
-
+      request(:get, messages_path)
     end
 
     private
 
-    def request(path)
-      #http client request goes here
+    def request(type, path)
+      uri = URI.join(@base_url, path)
+
+      session = Net::HTTP.new(uri.host, uri.port)
+      session.use_ssl = (uri.scheme == 'https')
+      session.open_timeout = @timeout
+      session.read_timeout = @timeout
+
+      req = request_type(type).new(uri.path)
+      req.basic_auth @username, @password
+
+      session.start do |http|
+        http.request(req)
+      end
     end
 
     def message_path(message_id)
@@ -30,6 +42,15 @@ module CustomerioAPI
 
     def messages_path
       "/v1/api/messages"
+    end
+
+    def request_type(type)
+      case type
+      when :get
+        Net::HTTP::Get
+      else
+        # bad stuff
+      end
     end
   end
 end
